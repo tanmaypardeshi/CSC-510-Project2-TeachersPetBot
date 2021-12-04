6b8b83a499a9dd588dbb0b430618fe6fed0a9014
Chandrahas Reddy Mandapati <chandrahasreddymandapati@Chandrahass-MBP.lan>
2021-12-04 17:51:11 -0500

    chaning the order of tests

--- test/tests.py
+++ test/tests.py
@@ -44,10 +44,10 @@ async def run_tests():
         await test_help.test(testing_bot, TEST_GUILD_ID)
         print('testing email address configuration\n----------')
         await test_email_address.test(testing_bot, TEST_GUILD_ID)
-        print('testing email utility\n-----------')
-        await test_email_utility.test()
         print('testing chart\n-----------')
         await test_chart.test(testing_bot, TEST_GUILD_ID)
+        print('testing email utility\n-----------')
+        await test_email_utility.test()
     except AssertionError as ex:
         print('exception: ', type(ex).__name__ + ':', ex)
         print('--')
