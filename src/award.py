d0c922f6862d52efd03081e9cbc382ba9544f964
SuryaUpadyayula <upadyayulasurya@gmail.com>
2023-11-26 18:59:19 -0500

    removed unnecessary lines from code

--- src/award.py
+++ src/award.py
@@ -2,10 +2,8 @@ def award_update_rank_and_xp(rank, current_xp, awarded_xp):
     quotient, remainder = divmod(awarded_xp, 100)
     rank += quotient
     current_xp += remainder
-    
     if current_xp >= 100:
         quotient, remainder = divmod(current_xp, 100)
         rank += quotient
         current_xp = remainder
-    
     return rank, current_xp
