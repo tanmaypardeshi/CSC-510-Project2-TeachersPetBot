f12273b19b9674ebd98d305a21a5ddb0c5fabe81
SuryaUpadyayula <upadyayulasurya@gmail.com>
2023-11-26 18:51:17 -0500

    fixed bug in penalize.py

--- src/penalize.py
+++ src/penalize.py
@@ -1,17 +1,25 @@
 def penalize_update_rank_and_xp(rank, xp, penalty):   
 
-    deduct_from_rank = penalty // 100
-    deduct_from_xp = penalty % 100
-           
-    new_rank = max(rank - deduct_from_rank, 0)  
-    new_xp = xp - deduct_from_xp
-
-    if new_xp < 0 and new_rank == 0:
+    total_points = rank * 100 + xp
+    #print(total_points)
+    #deduct_from_rank = penalty // 100
+    #deduct_from_xp = penalty % 100
+    updated_points = total_points - penalty
+    #print(updated_points)       
+    #new_rank = max(rank - deduct_from_rank, 0)  
+    #new_xp = xp - deduct_from_xp
+    if updated_points <= 0:
         new_rank = 0
         new_xp = 0
-    elif new_xp < 0 and new_rank > 0:
-        new_rank -= 1
-        new_xp = 100 - abs(new_xp)
+    elif updated_points > 0:
+        new_rank = updated_points // 100
+        new_xp = updated_points % 100
+    # if new_xp < 0 and new_rank == 0:
+    #     new_rank = 0
+    #     new_xp = 0
+    # elif new_xp < 0 and new_rank > 0:
+    #     new_rank -= 1
+    #     new_xp = 100 - abs(new_xp)
     return new_rank, new_xp
 
-print(penalize_update_rank_and_xp(2, 90, 90)) # (0, 95)
+print(penalize_update_rank_and_xp(3, 90, 400)) # (0, 95)
