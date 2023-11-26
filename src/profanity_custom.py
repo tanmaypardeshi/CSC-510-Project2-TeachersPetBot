6e258ae32d7da3f7e950a8dbfe0a1146c14981b5
spillal2 <spillal2@ncsu.edu>
2023-11-26 12:01:14 -0500

    Penalizes the first two penalties

--- src/profanity_custom.py
+++ src/profanity_custom.py
@@ -34,6 +34,14 @@ async def set(ctx):
         except:
             await ctx.send('Invalid entry. Try again...')
             return
+        await ctx.send('How much penalty should a person get on the first two messages containing profanity')
+        try:
+            penalty_value = int((await BOT.wait_for('message', timeout=60.0, check=lambda x:
+            x.channel.name ==
+                'instructor-commands' and x.author.id != BOT.user.id)).content)
+        except:
+            await ctx.send('Invalid entry. Try again...')
+            return
         await ctx.send('How long should a user be placed in timeout when caught spamming? '
                         'Format: min,hours,days Example: 5,0,0 is 5 minutes')
         try:
@@ -61,9 +69,9 @@ async def set(ctx):
 
         db.mutation_query(
             'UPDATE profanity_settings SET warning_num = ?, timeout_num = ?, timeout_min = ?, '
-            'timeout_hour = ?, timeout_day = ?, time_between_clears = ?',
+            'timeout_hour = ?, timeout_day = ?, time_between_clears = ?, penalty_value = ?',
             [msg_warn_num, msg_timeout_num, int(timeout_time[0]), int(timeout_time[1]),
-             int(timeout_time[2]), msg_kicked_num]
+             int(timeout_time[2]), msg_kicked_num, penalty_value]
         )
         await ctx.send('Profanity Settings successfully updated!')
     else:
@@ -108,9 +116,10 @@ def init(bot):
         timeout_hour = 0  # hours in timeout
         timeout_day = 0  # days in timout
         kicked_out_violations = 3
+        penalty_value = 10
         db.mutation_query(
-            'INSERT INTO profanity_settings VALUES (?, ?, ?, ?, ?, ?)',
-            [warning_num, timeout_num, timeout_min, timeout_hour, timeout_day, kicked_out_violations]
+            'INSERT INTO profanity_settings VALUES (?, ?, ?, ?, ?, ?, ?)',
+            [warning_num, timeout_num, timeout_min, timeout_hour, timeout_day, kicked_out_violations, penalty_value]
         )
 
 ###########################
